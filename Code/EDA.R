9558bf4c5687c8c30694ef7c8fbad35e888d27a2
Bird Fish <mymelodys@live.cn>
2019-02-18 01:21:33 -0500

    close

--- Code/EDA.R
+++ Code/EDA.R
@@ -1,1 +1,150 @@
-library(pacman)
+# Created by Kunpeng Yang
+# Last updated 1/17 2019
+
+# import data 
+df <-read.csv("Data/bikeVendors.csv")
+
+# View data
+top_n(df, 10)
+
+# add a new column which is the average market share among all stores of a certain bike
+library(tidyverse)
+df <- df %>%
+  mutate(Avg = select(., Albuquerque.Cycles:Wichita.Speed) %>% rowSums(na.rm = TRUE)/30)
+
+levels(df$category1)
+levels(df$category2)
+levels(df$frame)
+
+# View data structure
+str(df)
+
+
+
+
+# Explore data -----------------------------------------------------------
+## by price ------------------------
+df1 <- df[order(df$price),] # sort data by price
+barplot(df1$price, names.arg=df1$model, xlab = "Bike model", ylab = "Price",
+        main = "Bike Price", col = "green", border = "white")
+
+boxplot(df$price, main = "Bike Price")
+
+boxplot(df$price~df$category1, ylab = "Average Price") # Mountain bike vs Road bike
+
+## by market share -----------------
+
+
+# select mountain bikes
+mountain <- subset(x = df, subset = category1 == "Mountain")
+# select road bikes
+road <- subset(x = df, subset = category1 == "Road")
+# plot mountain bike mkt shares
+plot(mountain$Avg, type = "o", col = "red", xlab = "Bike model", ylab = "Average Market Share",
+     main = "Average market share of bikes by categories")
+# add line of road bike shares
+lines(road$Avg, type = "o", col = "blue")
+
+# box plot
+boxplot(df$Avg ~ df$category1, ylab = "Average Market Share")
+
+
+
+## clusters--------------------------------------------------
+library(cluster)
+library(factoextra)
+
+# drop categorial data fields
+drops <- c("model", "category1", "category2", "frame")
+bike2 <- df
+bike2 <- bike2[, !(names(bike2) %in% drops)]
+
+# use bike models as row names
+bike_model <- df$model
+row.names(bike2) <- bike_model
+
+bike2$price <- as.numeric(bike2$price)
+
+bike2 <- na.omit(bike2) # remove missing value
+bike2 <- scale(bike2) # scaling to avoid influence of unit
+
+set.seed(123)
+
+fviz_nbclust(bike2, kmeans, method = "wss")
+
+# Average Silhousette Method 
+fviz_nbclust(bike2, kmeans, method = "silhouette")
+
+# Gap Statistic Method 
+gap_stat <- clusGap(bike2, FUN = kmeans, nstart = 25,
+                    K.max = 10, B = 50)
+fviz_gap_stat(gap_stat)
+
+# use 4 clusters
+k4 <- kmeans(bike2, centers = 4, nstart = 25)
+fviz_cluster(k4, data = bike2)
+
+
+# Price ~ Category t-test ----------------------------------------------
+# check dependent variable distribution
+plot(density(df$price))
+
+# check skewness
+library(moments)
+agostino.test(df$price)
+# fix positive skew
+agostino.test(log(df$price))
+
+# build model
+price_category <- t.test(log(price) ~ category1, data = df)
+price_category
+
+# Price ~ Frame t test ----------------------------------------------
+# View average price of different types of bikes 
+boxplot(df$price~df$frame, ylab = "Average Price")
+
+# check distribution
+plot(density(df$price))
+
+# check skewness
+library(moments)
+agostino.test(df$price)
+# fix positive skew
+agostino.test(log(df$price))
+
+# build model
+price_category <- t.test(log(price) ~ frame, data = df)
+price_category
+
+# Which model has the highest market share -----------------------------------------
+
+# Adding a colume of average market share
+# row sum is divided by 30 because there are 30 stores in the dataset
+library(tidyverse)
+df <- df %>%
+  mutate(Avg_percentage = select(., Albuquerque.Cycles:Wichita.Speed) %>% rowSums(na.rm = TRUE)*100/30)
+
+# sort data by average market share
+sorted_mkt <- df[order(df$Avg_percentage, decreasing = TRUE),] 
+
+sorted_mkt[, c("model", "category1", "price", "Avg_percentage" )]
+
+# Market share ~ price ####
+# Distribution of dependent variable
+plot(density(df$Avg_percentage))
+agostino.test(df$Avg_percentage)
+
+agostino.test(log(df$price))
+
+# identify relationship between predictor and dependent variable
+library(car)
+scatterplot(df$price, df$Avg_percentage)
+
+# linear regression model
+mkt_model <- lm(df$Avg_percentage ~ df$price)
+summary(mkt_model)
+
+# check residuals
+qqnorm(mkt_model$residuals)
+qqline(mkt_model$residuals)
+shapiro.test(mkt_model$residuals)
--- Portfolio/Week 12 cluster.R
+++ Portfolio/Week 12 cluster.R
@@ -1,3 +1,6 @@
+# Created by Kunpeng Yang
+# Last updated 1/17 2019
+
 # load packages
 library(tidyverse)
 library(cluster)
@@ -15,3 +18,38 @@ fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4
 # K-means ####
 k4 <- kmeans(df, centers = 4, nstart = 25) # group the data into 4 clusters
 fviz_cluster(k4, data = df) # plot the clusters
+
+# multiple plots of different clusters
+k2 <- kmeans(df, centers = 2, nstart = 25) # set clusters to 2
+k3 <- kmeans(df, centers = 3, nstart = 25) # set clusters to 3
+k4 <- kmeans(df, centers = 4, nstart = 25) # set clusters to 4
+k5 <- kmeans(df, centers = 5, nstart = 25) # set clusters to 5
+
+p1 <- fviz_cluster(k2, geom = "point", data = df) + ggtitle("k = 2")
+p2 <- fviz_cluster(k3, geom = "point", data = df) + ggtitle("k = 3")
+p3 <- fviz_cluster(k4, geom = "point", data = df) + ggtitle("k = 4")
+p4 <- fviz_cluster(k5, geom = "point", data = df) + ggtitle("k = 5")
+
+library(gridExtra)
+grid.arrange(p1, p2, p3, p4, nrow=2)
+
+# Find optimal clusters ####
+set.seed(123)
+
+fviz_nbclust(df, kmeans, method = "wss")
+
+# Average Silhousette Method ####
+fviz_nbclust(df, kmeans, method = "silhouette")
+
+# Gap Statistic Method ####
+gap_stat <- clusGap(df, FUN = kmeans, nstart = 25,
+                    K.max = 10, B = 50)
+fviz_gap_stat(gap_stat)
+
+# Extracting Results ####
+# Analysis above found the optimal number of clusters is 9 or 10
+# I will perform final analysis with 9 clusters
+set.seed(111)
+final <- kmeans(df, 9, nstart = 25)
+print(final) # print out the result
+fviz_cluster(final, data = df) # plot the clusters
